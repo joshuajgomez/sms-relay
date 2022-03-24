23617c38c4bdc3164549198c7233390a0ffe9e27
Joshua Gomez <joshuajgomez@gmail.com>
2022-03-24 23:13:45 +0900

    Correct date format

--- app/src/main/java/com/joshgm3z/smsrelay/utils/DateUtil.kt
+++ app/src/main/java/com/joshgm3z/smsrelay/utils/DateUtil.kt
@@ -7,52 +7,15 @@ import java.util.*
 class DateUtil {
 
     companion object {
-        fun getTextDate(dateInstance: Long): String {
-            val instance = Calendar.getInstance()
-            instance.time = Date(dateInstance)
-            val month: String = getMonth(instance)
-            val day = instance.get(Calendar.DAY_OF_MONTH)
-            val year = instance.get(Calendar.YEAR)
-            val minute = instance.get(Calendar.MINUTE)
-            val hour = instance.get(Calendar.HOUR)
-            val am_pm = instance.get(Calendar.AM_PM)
 
-            var outputDate: String
-            if (DateUtils.isToday(dateInstance)) {
-                outputDate = "$hour:$minute$am_pm"
-            } else {
-                outputDate = "$month $day, $year"
-            }
-
-            return outputDate
-        }
-
-        fun getTextDate2(instance: Long): String {
+          fun getTextDate(instance: Long): String {
             val date = Date(instance)
             val sdf: SimpleDateFormat = if (DateUtils.isToday(instance)) {
                 SimpleDateFormat("h:mm aa")
             } else {
-                SimpleDateFormat("MMM DD, YYYY")
+                SimpleDateFormat("MMM dd, yyy")
             }
             return sdf.format(date).toLowerCase()
-        }
-
-        private fun getMonth(instance: Calendar): String {
-            when (instance.get(Calendar.MONTH)) {
-                1 -> return "jan"
-                2 -> return "feb"
-                3 -> return "mar"
-                4 -> return "apr"
-                5 -> return "may"
-                6 -> return "jun"
-                7 -> return "jul"
-                8 -> return "aug"
-                9 -> return "sep"
-                10 -> return "oct"
-                11 -> return "nov"
-                12 -> return "dec"
-            }
-            return ""
         }
     }
 
